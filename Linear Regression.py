70a7c9d41248daf2f547081e6b7d1b532cd248a3
ChrisAlves93 <30635471+ChrisAlves93@users.noreply.github.com>
2023-11-22 23:49:08 -0500

    Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import scipy.stats as sc
 import pandas as pd
@@ -9,4 +7,3 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
 dataset = pd.read_csv("h/Users/dataset_Facebook.csv")
-
